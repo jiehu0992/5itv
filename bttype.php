b42fa931fc89f01cda2b04ff22c5434d26af32a9
jiehu0992 <1517152664@qq.com>
2023-04-16 09:27:26 +0000

    update bttype.php. mysqli  女性显示为粉色
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- bttype.php
+++ bttype.php
@@ -1,68 +1,61 @@
 <?php
-include("conn.php");//数据库连接
-$array=array();
-$sql=mysql_query("select * from tree_lr");
-		while ($row=mysql_fetch_array($sql)){
-			$array[]=$row;         //查出数据保存到数组中
-		}
-
-
-//后台左侧循环树形栏目
-function lefttree(){
-	global $array; //设置全局变量
-	$tree = array();
-	$categorylist="";
-	if( $array ){
-		foreach ( $array as $v ){
-			//$pt = $v['pid'];
-			$pt = $v['pid'];
-			$list = @$tree[$pt] ? $tree[$pt] : array();
-			array_push( $list, $v );
-			$tree[$pt] = $list;
-		}
-	}
-	if(is_array($tree[0])){
-		$i = 0;
-		foreach($tree[0] as $k=>$v){
-			$i++;
-			if($tree[$v["id"]]){
-				$categorylist.="<li><a href=\"#\">".$v["name"]."</a>";
-				$categorylist.="<ul>\n";
-				$categorylist.=sonTree($tree[$v["id"]],$tree,0,$type);
-				$categorylist.="</ul>\n";
-				$categorylist.="</li>\n";
-			}else{
-				if($v["is_link"]==0){
-					$categorylist.="<li><a href=\"#\">".$v["name"]."</a>";
-				}
-			}
-		}
-	}
-	return $categorylist;
+// Database connection using mysqli
+$mysqli = new mysqli("127.0.0.1","root","root", "Database");
+if ($mysqli->connect_errno) {
+    die("Failed to connect to MySQL: " . $mysqli->connect_error);
 }
 
+// Prepare statement
+$sql = "SELECT * FROM tree_lr";
+$stmt = $mysqli->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
 
-function sonTree($arr,$tree,$level,$type){
-	$level++;
-	$ii=0;
-	foreach($arr as $k2=>$v2){
-		$ii++;
-
-		if($tree[$v2["id"]]){
-			$categorylist.="<li><a href=\"#\">".$v2["name"]."</a>\n";
-			$categorylist.="<ul>\n";
-			$categorylist.=sonTree($tree[$v2["id"]],$tree,$level,$type);
-			$categorylist.="</ul>\n";
-			$categorylist.="</li>\n";
-		}else{
-			if($v["is_link"]==0){
-				$categorylist.="<li><a href=\"#\">".$v2["name"]."</a></li>";
-			}
-		}
-	}
-	return $categorylist;
+// Fetch results into an array
+$array = array();
+while ($row = $result->fetch_assoc()) {
+    $array[] = $row;
 }
 
-$menu=lefttree();//调用函数
+// Optimize the lefttree() function
+function lefttree($array){
+    $tree = array();
+    $categorylist = "";
+    foreach ($array as $v) {
+        $list = @$tree[$v['pid']] ?: array();
+        $list[] = $v;
+        $tree[$v['pid']] = $list;
+    }
+    if (is_array($tree[0])) {
+        $categorylist = "<ul>\n";
+        $categorylist .= sonTree($tree[0], $tree);
+        $categorylist .= "</ul>\n";
+    }
+    return $categorylist;
+}
 
+function sonTree($arr, $tree, $level = 1) {
+    $categorylist = "";
+    foreach ($arr as $k => $v) {
+        if ($tree[$v['id']]) {
+            $categorylist .= "<li><a href=\"#\" style='color: ";
+            $categorylist .= ($v['sex'] == '女') ? 'pink' : 'black';
+            $categorylist .= ";'>{$v['name']}</a>\n";
+            $categorylist .= "<ul>\n";
+            $categorylist .= sonTree($tree[$v['id']], $tree, $level + 1);
+            $categorylist .= "</ul>\n";
+            $categorylist .= "</li>\n";
+        } else {
+            if ($v['is_link'] == 0) {
+                $categorylist .= "<li><a href=\"#\" style='color: ";
+                $categorylist .= ($v['sex'] == '女') ? 'pink' : 'black';
+                $categorylist .= ";'>{$v['name']}</a></li>\n";
+            }
+        }
+    }
+    return $categorylist;
+}
+
+// Generate the menu
+$menu = lefttree($array);
 ?>
