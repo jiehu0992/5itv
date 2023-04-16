e00ad12303e0aec76aa8ff9c27f293a4518e11ca
jiehu0992 <1517152664@qq.com>
2023-04-16 12:41:47 +0000

    update tree.php. 修改为支出mysqli php5.6  同时将女性颜色显示为#ff1493粉色，增加人数统计
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- tree.php
+++ tree.php
@@ -1,10 +1,33 @@
 <?php
 include("conn.php");//数据库连接
-$array=array();
-$sql=mysql_query("select * from tree_lr");
-		while ($row=mysql_fetch_array($sql)){
-			$array[]=$row;         //查出数据保存到数组中
-		}
+// Prepare the query to get the total number of rows and max dc
+$stmt = mysqli_prepare($link, "SELECT COUNT(*) AS total, MAX(dc) AS maxdc FROM tree_lr WHERE name NOT LIKE ?");
+$search_term = '%出%';
+mysqli_stmt_bind_param($stmt, 's', $search_term);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_result($stmt, $total, $maxdc);
+mysqli_stmt_fetch($stmt);
+mysqli_stmt_close($stmt);
+
+// Prepare the query to fetch the required data
+$stmt = mysqli_prepare($link, "SELECT id, name, pid, sex, dc FROM tree_lr");
+mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_result($stmt, $id, $name, $pid, $sex, $dc);
+
+$array = array();
+while (mysqli_stmt_fetch($stmt)) {
+    $array[] = array(
+        'id' => $id,
+        'name' => $name,
+        'pid' => $pid,
+        'sex' => $sex,
+        'dc' => $dc
+    );
+}
+mysqli_stmt_close($stmt);
+
+echo "<div style='position: fixed;margin: 0 auto;width: 100%;top: 0.6rem;z-index: -1;text-align: right;color: #1b7ac5;'>截至今日，本族共繁衍 ".$maxdc." 代，总计 ".$total." 人。</div>";
+
 
 //后台左侧循环树形栏目
 function lefttree(){
@@ -42,27 +65,29 @@ function lefttree(){
 }
 
 
-function sonTree($arr,$tree,$level,$type){
-	$level++;
-	$ii=0;
-	foreach($arr as $k2=>$v2){
-		$ii++;
+function sonTree($arr, $tree, $level, $type) {
+    $level++;
+    $ii = 0;
+    foreach ($arr as $k2 => $v2) {
+        $ii++;
 
-		if($tree[$v2["id"]]){
-			$categorylist.="<li><span><i class=\"icon-minus-sign\"></i>".$v2["name"]."</span>\n";
-			$categorylist.="<a href=\"info.php?id=".$v2["id"]."\" target=\"_blank\">详细</a><ul>\n";
-			$categorylist.=sonTree($tree[$v2["id"]],$tree,$level,$type);
-			$categorylist.="</ul>\n";
-			$categorylist.="</li>\n";
-		}else{
-			if($v["is_link"]==0){
-			/*	$categorylist.="<li><span><i class=\"icon-minus-sign\"></i>".$v2["name"]."</span>\n字辈：<font color=\"red\">".$v2["zibei"]."</font>，刘氏第<font color=\"red\">".$v2["dc"]."</font>世代，妻子：<font color=\"red\">".$v2["wname"]."</font>。平生简介：".$v2["info"]."</li>";*/
-					$categorylist.="<li><span><i class=\"icon-minus-sign\"></i>".$v2["name"]."</span>\n<a href=\"info.php?id=".$v2["id"]."\" target=\"_blank\">详细</a>";
-			}
-		}
-	}
-	return $categorylist;
+        if ($tree[$v2["id"]]) {
+            $categorylist .= "<li><span><i class=\"icon-minus-sign\"></i>".$v2["name"]."</span>\n";
+            $categorylist .= "<a href=\"info.php?id=".$v2["id"]."\" target=\"_blank\">详细</a><ul>\n";
+            $categorylist .= sonTree($tree[$v2["id"]], $tree, $level, $type);
+            $categorylist .= "</ul>\n";
+            $categorylist .= "</li>\n";
+        } else {
+            if ($v["is_link"] == 0) {
+                $sex_color = ($v2["sex"] == "女") ? "color:#ff1493;" : "";
+                $categorylist .= "<li><span style=\"$sex_color\"><i class=\"icon-minus-sign\"></i>".$v2["name"]."</span>\n";
+                $categorylist .= "<a href=\"info.php?id=".$v2["id"]."\" target=\"_blank\">详细</a>";
+            }
+        }
+    }
+    return $categorylist;
 }
+
 
 
 $menu=lefttree();//调用函数
