9f3f49628b6792b3d5500fc2276c1228f46003ba
jiehu0992 <1517152664@qq.com>
2023-04-16 12:42:31 +0000

    update conn.php.
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("127.0.0.1", "root", "lsc606414lk", "demo");
+$link = mysqli_connect("127.0.0.1", "root", "root", "database");
 if (!$link) {
     die("连接失败: " . mysqli_connect_error());
 }
