c580b6547cea2c8812572935710d97cba31b31d7
jiehu0992 <1517152664@qq.com>
2023-04-16 14:36:20 +0000

    update houtai/index.php.
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- houtai/index.php
+++ houtai/index.php
@@ -1,15 +1,21 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
 <head>
+<title>刘氏家族族谱树</title>
+<meta name="keywords" content="刘氏族谱,族谱,刘氏家谱,家谱树">
+<meta name="description" content="">
+<meta http-equiv="X-UA-Compatible" content="chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="apple-mobile-web-app-capable" content="yes">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>刘三才后裔刘氏家族族谱树</title>
-<meta name="keywords" content="刘氏族谱,族谱,刘氏家谱,家谱树,爱视传媒">
-<meta name="description" content="自价、金二祖人川至今已近六百年历史，三才祖之后亦有四百多年。目前人数众多，分布省内外，很少联系往来，不少人祖宗、祖籍观念淡薄，上不知祖宗之根，下不知自已之源，甚至辈份都不清楚。虽然同根共祖具有相同血统和基因，但如同散沙，怎能互助互爱团结拼搏？在这世纪之交，在改革开放实行市场经济的形势下，我们不能辱没祖宗，一定要承上启下，继往开来，继承和发扬祖先耕读传家，拼搏不息，开拓进取的精神，以续谱为纽带，团结全族老小共同奋斗，从振兴家族做起，为中华的振兴作出贡献，为我氏在各族之林赢得一席之地">
 <link rel="stylesheet" type="text/css" href="/css/style.css" />
 <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
 <!--主要样式-->
-<script type="text/javascript" src="/js/context-menu.js"></script>
-<script type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="js/context-menu.js"></script>
+<script src="//cdn.bootcss.com/jquery/1.12.3/jquery.min.js"></script>
+<script src="/images/layer/layer.js" type="text/javascript"></script>
+<style type="text/css">.tree{background-color: transparent;width:100%}</style>
 <script type="text/javascript">
 $(function(){
     $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', '收起子孙族谱树');
@@ -25,29 +31,20 @@ $(function(){
         e.stopPropagation();
     });
 });
+function getid(a){id=(a.id)}$(document).ready(function(){$('.outside').click(function(){layer.open({type:2,title:false,shadeClose:true,shade:0.6,anim:1,area:['610px','100%'],content:[id,'no']})})})
 </script>
-	
+
 </head>
 <body>
-<nav class="navbar navbar-default" role="navigation">
-	<div class="container-fluid">
-	<div>
-		<ul class="nav navbar-nav">
-			<li><a href="/os.php">欧式族谱</a></li>		
-			<li><a href="/bt.php">塔式族谱</a></li>			
-			<li><a href="/ztree.html">ztree族谱树</a></li>			
-			<li><a href="/99pu.html">ztree族谱树84.3MB</a></li>	
-			<li><a href="/search.php">搜索</a></li>
-		</ul>
-	</div>
-	</div>
-</nav>
+    <?php
+	include_once 'header.php';
+?>
 	<div class="tree well">
 <?php include_once 'tree2.php';?>
 	<ul id="dhtmlgoodies_tree" class="dhtmlgoodies_tree">
     <?php echo $menu;?>
 	</ul>
 </div>
-<div align="center">2018年爱视传媒制作http://liu.5itv.org</div>
+<div align="center">2023年爱视传媒制作http://demo.tvsbar.com</div>
 </body>
 </html>
