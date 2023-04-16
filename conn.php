330a42acd65fc2d398a88006b2f160ada9adf358
jiehu0992 <1517152664@qq.com>
2023-04-16 12:40:00 +0000

    update conn.php. 修改mysql为mysqli
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
-$link=mysql_connect("localhost","root","root") or die("连接数据库服务器失败！".mysql_error()); //连接MySQL服务器
-$db=mysql_select_db("treelcopy",$link);//链接数据库
-mysql_query("set names utf8");//设置编码
-
+$link = mysqli_connect("127.0.0.1", "root", "lsc606414lk", "demo");
+if (!$link) {
+    die("连接失败: " . mysqli_connect_error());
+}
 ?>
