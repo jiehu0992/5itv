e2d16ccb79582f27ef5b5596a04f14747acc2623
jiehu0992 <1517152664@qq.com>
2023-04-17 13:36:13 +0000

    mysqli 数据库连接
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- houtai/conn.php
+++ houtai/conn.php
@@ -1,5 +1,27 @@
 <?php
-$link=mysql_connect("localhost","root","root") or die("连接数据库服务器失败！".mysql_error()); //连接MySQL服务器
-$db=mysql_select_db("treelcopy",$link);//链接数据库
-mysql_query("set names utf8");//设置编码
+$host = 'localhost';
+$user = 'root';
+$password = 'root';
+$dbname = 'dataname';
+
+// Create a new MySQLi object
+$mysqli = new mysqli($host, $user, $password, $dbname);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die('连接数据库服务器失败！' . $mysqli->connect_error);
+}
+
+// Set encoding to UTF-8
+if (!$mysqli->set_charset('utf8')) {
+    die('设置编码失败！');
+}
+/*
+<?php
+$link = mysqli_connect("127.0.0.1", "root", "root", "dbname");
+if (!$link) {
+    die("连接失败: " . mysqli_connect_error());
+}
+?>
+*/ 如果不成功就直接用下面这个
 ?>
