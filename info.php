31d480e3ff2312717228df6928fb47f1ff65d179
jiehu0992 <1517152664@qq.com>
2023-04-17 05:24:42 +0000

    update info.php. mysqli
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- info.php
+++ info.php
@@ -1,140 +1,111 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>刘三才后裔刘氏家族族谱树</title>
-<meta name="keywords" content="刘氏族谱,族谱,刘氏家谱,家谱树,爱视传媒">
-<meta name="description" content="自价、金二祖人川至今已近六百年历史，三才祖之后亦有四百多年。目前人数众多，分布省内外，很少联系往来，不少人祖宗、祖籍观念淡薄，上不知祖宗之根，下不知自已之源，甚至辈份都不清楚。虽然同根共祖具有相同血统和基因，但如同散沙，怎能互助互爱团结拼搏？在这世纪之交，在改革开放实行市场经济的形势下，我们不能辱没祖宗，一定要承上启下，继往开来，继承和发扬祖先耕读传家，拼搏不息，开拓进取的精神，以续谱为纽带，团结全族老小共同奋斗，从振兴家族做起，为中华的振兴作出贡献，为我氏在各族之林赢得一席之地">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-<!--主要样式-->
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-<style type="text/css">
-* {
-	margin: 0;
-	padding: 0;
-}
-.page {
-	width: 1000px;
-	height:500px;height:auto;min-height:500px;
-	margin: 0 auto;
-	border: 2px solid #000;
-	padding: 10px;
-}
-.top_line {
-	border-top: 1px solid #000;
-}
-.level {
-	width: inherit;
-	height: 500px;
-	border-left: 1px solid #000;
-	border-bottom: 1px solid #000;
-	border-right: 1px solid #000;
-	overflow: hidden;
-/*	writing-mode:tb-rl;
-    -webkit-writing-mode: vertical-rl;      
-    writing-mode: vertical-rl;
-    *writing-mode: tb-rl;*/
-
--ms-writing-mode: tb-rl; /* old syntax. IE */
--webkit-writing-mode: vertical-rl;
--moz-writing-mode: vertical-rl;
--ms-writing-mode: vertical-rl;
-writing-mode: vertical-rl; /* new syntax */
-			
--webkit-text-orientation: upright;
--moz-text-orientation: upright;
--ms-text-orientation: upright;
-text-orientation: upright;
-
-}
-.intro {
-	display: inline-block;
-	width: 100%;
-	height: inherit;
-}
-.level_name {
-    border-left: 1px solid #000;
-    display: inline-block;
-    font-size: 22px;
-    font-weight: bold;
-    height: inherit;
-    letter-spacing: 25px;
-    padding: 0 10px;
-    text-align: center;
-}
-.text_vertical {
-	width: 30px;
-	font-size: 16px;
-	word-wrap: break-word;
-	letter-spacing: 10px;
-}
-.first {
-	display: inline-block;
-	height: 180px
-}
-.second {
-	display: inline-block;
-	height: 180px
-}
-.member {
-    padding-right: 20px;
-    padding-top: 10px;
-}
-.member_name {
-
-    font-family: 黑体;
-	font-size: 25px;
-    font-weight: 900;
-    margin-left: 5px;
-	letter-spacing: 15px;
-}
-.member_desc {
-    text-indent: 40px;
-    letter-spacing:5px;
-    line-height: 125%;
-    font-family: "Microsoft YaHei", "黑体", "宋体", sans-serif;
-}
-
-.verticle-mode {
-    writing-mode: tb-rl;
-    -webkit-writing-mode: vertical-rl;
-    writing-mode: vertical-rl;
-    *writing-mode: tb-rl;
-}
-
-</style>
-
-
-</head>
-<body>
-	<div class="tree well">
-   <div class="page">
-<div class="level top_line">
-
 <?php
-include("conn.php");//数据库连接
-$id = (int)$_GET['id']; // 强制将传入的id参数转为整数类型，以防止SQL注入攻击
-$sql=mysql_query("select info,name,wname,dc,zibei,mudi from tree_lr where id='$id'");
-$datarow = mysql_num_rows($sql); 
- for($i=0;$i<$datarow;$i++){
-                $sql_arr = mysql_fetch_assoc($sql);
-                  $name = $sql_arr['name'];
-                  $wname = $sql_arr['wname'];
-                  $dc = $sql_arr['dc'];
-                  $zibei = $sql_arr['zibei'];
-                  $info = $sql_arr['info'];
-                  $mudi = $sql_arr['mudi'];
-                echo " <div class=\"level_name\">第<font color=\"red\">$dc</font>世代</div>
-                	<div class=\"member\">
-<div style=\"\" class=\"member_name\">刘$name</div>
-<div  class=\"member_desc\"><font color=\"red\">字辈:</font>$zibei </br><font color=\"red\">妻子:</font>$wname </br><font color=\"red\">生平:</font>$info</br>$mudi</div></div>";
-            }
-   
+    require "conn.php";
+    $id = $_GET['id'];
+    
+    // 创建预处理语句
+    $stmt = mysqli_prepare($link, "SELECT * FROM tree_lr WHERE id=?");
+    
+    // 绑定参数并执行查询
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    
+    // 获取查询结果
+    $result = mysqli_stmt_get_result($stmt);
+    $sql_arr = mysqli_fetch_assoc($result);
 ?>
 
-		</div>
-		</div>
-			</div>
-<div align="center">2018年爱视传媒制作http://demo.tvsbar.com</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>查看详情_刘三才族裔刘氏族谱</title>
+</head>
+	<link rel="stylesheet" type="text/css" href="/css/style.css" />
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
+<!--主要样式-->
+<script type="text/javascript" src="/js/context-menu.js"></script>
+<script type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
+<body>
+
+	<div class="tree well">
+	<div align=center>
+    刘<?php echo $sql_arr['name']?>编号:</label><input type="text" name="id" value="<?php echo $sql_arr['id']?>" readonly="readonly">
+    刘<?php echo $sql_arr['name']?>父亲编号：</label><input type="text" name="pid" value="<?php echo $sql_arr['pid']?>" readonly="readonly">
+    <table align="center" cellpadding="0" cellspacing="0" style="width: 80%;height:100%">
+
+    <tr>
+    <td>
+    	<p>
+    		姓名：<?php echo $sql_arr['name']?>
+    	</p>
+		<p>
+    		性别：<?php echo $sql_arr['sex']?>&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;
+    		配偶：<?php echo $sql_arr['wname']?>
+    	</p>
+		<p>
+    		排行:<?php echo $sql_arr['rank']?>&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
+			世代：<?php echo $sql_arr['dc']?>
+    	</p>
+		<p>	字辈：<?php echo $sql_arr['zibei']?>&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
+            祖父：<?php echo $sql_arr['gdad']?>
+
+    	</p>
+		<p>
+            父亲：<?php echo $sql_arr['dad']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+            母亲：<?php echo $sql_arr['mother']?>
+		</p>
+        <p>
+            兄弟：<?php echo $sql_arr['brother']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+            姐妹：<?php echo $sql_arr['sisters']?>
+        </p>
+
+        <p>
+            儿子：<?php echo $sql_arr['son']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+            女儿：<?php echo $sql_arr['daughter']?>
+        </p>
+		<p>
+			生平：<?php echo $sql_arr['info']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+            其它：<?php echo $sql_arr['other']?>
+		</p>
+		<p>
+			国籍：<?php echo $sql_arr['nation']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			籍贯：<?php echo $sql_arr['jiguan']?>
+    	</p>
+		<p>
+			字：<?php echo $sql_arr['zi']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			号：<?php echo $sql_arr['hao']?>
+		</p>
+		<p>
+			曾用名：<?php echo $sql_arr['rname']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			生日:<?php echo $sql_arr['year']?>
+    	</p>
+		<p>
+			出生地：<?php echo $sql_arr['birthadresse']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			居住地：<?php echo $sql_arr['lifeadresse']?>
+		</p>
+		<p>
+			通讯：<?php echo $sql_arr['tongxun']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			婚姻：<?php echo $sql_arr['marriage']?>
+    	</p>
+		<p>
+			姐妹：<?php echo $sql_arr['sisters']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			继嗣：<?php echo $sql_arr['jisi']?>
+    	</p>
+		<p>
+			职业：<?php echo $sql_arr['zhiye']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			学历：<?php echo $sql_arr['xueli']?>
+    	</p>
+		<p>
+			祭日：<?php echo $sql_arr['dieday']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			墓地：<?php echo $sql_arr['mudi']?>
+		</p>
+		<p>
+			统计：<?php echo $sql_arr['tongji']?>
+		</p>
+    	</td>
+    	</tr>
+		</table>
+		   		</div>
+   			</div>
 </body>
 </html>
