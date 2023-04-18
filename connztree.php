06d88f77f63c6f0db8f9903912cca245f4cbd9ef
jiehu0992 <1517152664@qq.com>
2023-04-18 01:43:49 +0000

    update connztree.php.
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- connztree.php
+++ connztree.php
@@ -1,24 +1,28 @@
 <?php
-$mysqli  = new  mysqli ( 'localhost' ,  'root' ,  'root' ,  'tree' );
-mysqli_query($mysqli, "set names gbk");
-if ( $mysqli -> connect_error ) {
-    die(  $mysqli -> connect_errno + $mysqli -> connect_error );
+$mysqli = mysqli_connect('localhost', 'root', 'root', 'dataname');
+mysqli_query($mysqli, "SET NAMES gbk");
+if (!$mysqli) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
 }
-$sql='select * from tree_lr';
-$res=mysqli_query($mysqli, $sql);
-$array=array();
-while ($row=mysqli_fetch_row($res)){
-    $menu=array(
-        "id"=>$row[0],
-        "pId"=>$row[1],
-        "name"=>urlencode(iconv('gb2312','utf-8',$row[2])),
-        "open"=>1,
-        "file"=>$row[4],
+$sql = 'SELECT * FROM tree_lr';
+$res = mysqli_query($mysqli, $sql);
+
+if (!$res) {
+    die('Error: ' . mysqli_error($mysqli));
+}
+
+$array = array();
+while ($row = mysqli_fetch_row($res)) {
+    $menu = array(
+        "id" => $row[0],
+        "pId" => $row[1],
+        "name" => urlencode(iconv('gbk', 'utf-8', $row[2])),
+        "open" => 1,
+        "file" => $row[4],
     );
     array_push($array, $menu);
 }
 echo json_encode($array);
 
- $mysqli -> close ();
-
+mysqli_close($mysqli);
 ?>
