d025a1aa63517064c4f77a8dac2e49668b10ed24
jiehu0992 <1517152664@qq.com>
2023-04-19 01:38:45 +0000

    优化跳转样式
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- houtai/action-editinfo2.php
+++ houtai/action-editinfo2.php
@@ -39,18 +39,63 @@ $info	=$_POST['info'];
 
 // 更新数据
 $res=mysqli_query($link, "UPDATE tree_lr SET pid='$pid',name='$name',sex='$sex',wname='$wname',dad='$dad',gdad='$gdad',dc='$dc',mother='$mother',zibei='$zibei',rank='$rank',brother='$brother',sisters='$sisters',son='$son',daughter='$daughter',year='$year',dieday='$dieday',lifeadresse='$lifeadresse',mudi='$mudi',tongxun='$tongxun',nation='$nation',birthadresse='$birthadresse',zi='$zi',hao='$hao',jiguan='$jiguan',rname='$rname',marriage='$marriage',zhiye='$zhiye',xueli='$xueli',other='$other',jisi='$jisi',tongji='$tongji',info='$info' WHERE id=".$id) or die('修改数据出错：'.mysqli_error($link)); 
-if($res){
-	//这是代表成功
-	//这里就可以用跳转方法了 比如你的header重定向
-	$url='info2?id='.$id;
-	echo '编辑成功！';
-	header('Location:'.$url);
+  if($res){
+      //编辑成功，跳转到info2.php并传递参数id
+      $url = 'info2.php?id='.$id;
+      $message = '编辑成功！';
+    }else{
+      //编辑失败，返回上一页infoedit2.php并传递参数id
+      $url = 'infoedit2.php?id='.$id;
+      $message = '编辑失败！';
+    }
+    ?>
 
-}else{
-	//也可以返回上一步
-	$url='infoedit2.php';
-	echo '编辑失败！';
-	header('Location:'.$url);
-
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>确定</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      font-size: 16px;
+      line-height: 1.6;
+      background-color: #f6f6f6;
+      color: #333;
+      text-align: center;
+    }
+    h1 {
+      margin-top: 50px;
+      font-size: 24px;
+      font-weight: bold;
+    }
+    p {
+      margin-top: 20px;
+      font-size: 18px;
+      font-weight: bold;
+    }
+    .button {
+      margin-top: 40px;
+      padding: 10px 20px;
+      font-size: 18px;
+      font-weight: bold;
+      background-color: #4CAF50;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+    .button:hover {
+      background-color: #3e8e41;
+    }
+  </style>
+</head>
+<body>
+  <h1>确定</h1>
+  <p><?php echo $message ?></p>
+  <form action="<?php echo $url ?>" method="get">
+    <input type="hidden" name="id" value="<?php echo $id ?>">
+    <button type="submit" class="button">确定</button>
+  </form>
+</body>
+</html>
