77a4fc6354e737aca627b0bbd55dfd07d590bf89
jiehu0992 <1517152664@qq.com>
2023-04-21 14:49:59 +0000

    家谱树核心代码 二叉树左右值 php7.4
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- houtai/tree2.php
+++ houtai/tree2.php
@@ -27,7 +27,6 @@ mysqli_stmt_close($stmt);
 
 echo "<div style='position: fixed;margin: 0 auto;width: 100%;top: 0.6rem;z-index: -1;text-align: right;color: #1b7ac5;'>截至今日，本族共繁衍 ".$maxdc." 代，总计 ".$total." 人。</div>";
 
-
 //后台左侧循环树形栏目
 function lefttree(){
 	global $array; //设置全局变量
@@ -62,32 +61,28 @@ function lefttree(){
 	}
 	return $categorylist;
 }
-
-
-function sonTree($arr, $tree, $level, $type, $mysqli) {
+function sonTree($arr, $tree, $level, $type) {
     $level++;
     $ii = 0;
     $categorylist = '';
     foreach ($arr as $k2 => $v2) {
         $ii++;
         if ($tree[$v2["id"]]) {
-            $categorylist .= "<li><span><i class=\"icon-minus-sign\"></i>" . $v2["name"] . "</span>\n";
-            $categorylist .= "编号<font color=\"red\">" . $v2["id"] . "</font>，<a href=\"info2.php?id=" . $v2["id"] . "\" target=\"_blank\">生平简介</a>\n<a href=\"infoedit2.php?id=" . $v2["id"] . "\" target=\"_blank\">编辑详细</a>\n<a href=\"add.php\" target=\"_blank\">添加</a>\n第" . $v2["dc"] . "代" . $v2["zibei"] . "<ul>";
-            $categorylist .= sonTree($tree[$v2["id"]], $tree, $level, $type, $mysqli);
+            $categorylist .= "<li><span><i class='icon-minus-sign'></i>" . $v2["name"] . "</span>\n";
+            $categorylist .= "编号<font color='red'>" . $v2["id"] . "</font>，<a href='info2.php?id=" . $v2["id"] . "' target='_blank'>生平简介</a>\n<a href='infoedit2.php?id=" . $v2["id"] . "' target='_blank'>编辑详细</a>\n<a href='add.php' target='_blank'>添加</a>\n第" . $v2["dc"] . "代" . $v2["zibei"] . "<ul>";
+            $categorylist .= sonTree($tree[$v2["id"]], $tree, $level, $type);
             $categorylist .= "</ul>\n";
             $categorylist .= "</li>\n";
         } else {
             if ($v2["is_link"] == 0) {
                 $color = ($v2["sex"] == '女') ? "#ff1493" : ""; // check if the sex value is female, then set the color to #ff1493
-                $categorylist .= "<li><span style=\"color:" . $color . "\"><i class=\"icon-minus-sign\"></i>" . $v2["name"] . "</span>\n编号<font color=\"red\">" . $v2["id"] . "</font>，<a href=\"info2.php?id=" . $v2["id"] . "\" target=\"_blank\">生平简介</a>\n<a href=\"infoedit2.php?id=" . $v2["id"] . "\" target=\"_blank\">编辑详细</a>\n<a href=\"add.php\" target=\"_blank\">添加</a>\n第" . $v2["dc"] . "代" . $v2["zibei"] . "";
+                $categorylist .= "<li><span style='color:" . $color . "'><i class='icon-minus-sign'></i>" . $v2["name"] . "</span>\n编号<font color='red'>" . $v2["id"] . "</font>，<a href='info2.php?id=" . $v2["id"] . "' target='_blank'>生平简介</a>\n<a href='infoedit2.php?id=" . $v2["id"] . "' target='_blank'>编辑详细</a>\n<a href='add.php' target='_blank'>添加</a>\n第" . $v2["dc"] . "代" . $v2["zibei"] . "";
             }
         }
     }
     return $categorylist;
 }
 
-
-
-$menu=lefttree();//调用函数
-
+$menu = lefttree(); //调用函数
+  mysqli_close($link); //关闭数据库连接
 ?>
