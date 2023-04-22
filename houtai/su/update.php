cc6db1fb1f5d01b9b68af3f80bace0b5075c08c7
jiehu0992 <1517152664@qq.com>
2023-04-22 11:48:49 +0000

    改
    Signed-off-by: jiehu0992 <1517152664@qq.com>

---
+++ houtai/su/update.php
@@ -0,0 +1,15 @@
+<?php
+// Connect to the database
+$link = mysqli_connect("127.0.0.1", "root", "lsc606414lk", "demo");
+
+// Get the node's id and new name from the POST data
+$id = $_POST["id"];
+$name = $_POST["name"];
+
+// Update the node's name in the database
+$query = "UPDATE tree_lr SET name='" . mysqli_real_escape_string($link, $name) . "' WHERE id=" . intval($id);
+mysqli_query($link, $query);
+
+// Close the database connection
+mysqli_close($link);
+?>
