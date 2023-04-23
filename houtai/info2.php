e7a5de5a3e57c5bf9cfdbed315c394329ff7f402
jiehu0992 <1517152664@qq.com>
2023-04-23 15:36:10 +0000

    后台详情页面 增加权限验证账号admin密码123 php5.7
    Signed-off-by: jiehu0992 <1517152664@qq.com>

--- houtai/info2.php
+++ houtai/info2.php
@@ -1,64 +1,61 @@
-<?php
-    $link = mysqli_connect("127.0.0.1", "root", "root", "database");
-    if (!$link) {
-        die("连接失败: " . mysqli_connect_error());
-    }
-    $id = $_GET['id'];
-    $sql = mysqli_query($link,"SELECT * FROM tree_lr WHERE id=$id");
-    $sql_arr = mysqli_fetch_assoc($sql);
-?>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>刘三才后裔刘氏家族族谱树</title>
-	<meta name="keywords" content="刘氏族谱,族谱,刘氏家谱,家谱树,爱视传媒">
-	<meta name="description" content="自价、金二祖人川至今已近六百年历史，三才祖之后亦有四百多年。目前人数众多，分布省内外，很少联系往来，不少人祖宗、祖籍观念淡薄，上不知祖宗之根，下不知自已之源，甚至辈份都不清楚。虽然同根共祖具有相同血统和基因，但如同散沙，怎能互助互爱团结拼搏？在这世纪之交，在改革开放实行市场经济的形势下，我们不能辱没祖宗，一定要承上启下，继往开来，继承和发扬祖先耕读传家，拼搏不息，开拓进取的精神，以续谱为纽带，团结全族老小共同奋斗，从振兴家族做起，为中华的振兴作出贡献，为我氏在各族之林赢得一席之地">
-	<link href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="/css/info.css" />
+    <meta charset="UTF-8">
+    <title>编辑详情</title>
 </head>
+	<link rel="stylesheet" type="text/css" href="/css/style.css" />
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
 <!--主要样式-->
 <script type="text/javascript" src="/js/context-menu.js"></script>
 <script type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
 <body>
-  <div class="tree well">
-    <div align="center">
-      <label><red>刘<?php echo $sql_arr['name']?></red>编号:</label>
-      <input type="text" name="id" value="<?php echo $sql_arr['id']?>" readonly="readonly">
-      <label><red>刘<?php echo $sql_arr['name']?></red>父亲编号：</label>
-      <input type="text" name="pid" value="<?php echo $sql_arr['pid']?>" readonly="readonly">
-      <table align="center" cellpadding="0" cellspacing="0" style="width: 80%;height:100%">
-        <tr>
-          <td>
-            	<p>姓名：<?php echo $sql_arr['name']?></p>
-						<p>性别：<?php echo $sql_arr['sex']?>&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;配偶：<?php echo $sql_arr['wname']?></p>
-						<p>排行:<?php echo $sql_arr['rank']?>&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;父亲：<?php echo $sql_arr['dad']?></p>
-						<p>世代：<?php echo $sql_arr['dc']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;祖父：<?php echo $sql_arr['gdad']?></p>
-						<p>字辈：<?php echo $sql_arr['zibei']?>&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;兄弟：<?php echo $sql_arr['brother']?></p>
-						<p>生平：<?php echo $sql_arr['info']?></p>
-						<p>国籍：<?php echo $sql_arr['nation']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;籍贯：<?php echo $sql_arr['jiguan']?></p>
-						<p>字：<?php echo $sql_arr['zi']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;号：<?php echo $sql_arr['hao']?></p>
-						<p>曾用名：<?php echo $sql_arr['rname']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;生日:<?php echo $sql_arr['year']?></p>
-						<p>出生地：<?php echo $sql_arr['birthadresse']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;居住地：<?php echo $sql_arr['lifeadresse']?></p>
-						<p>通讯：<?php echo $sql_arr['tongxun']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;婚姻：<?php echo $sql_arr['marriage']?></p>
-						<p>姐妹：<?php echo $sql_arr['sisters']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;继嗣：<?php echo $sql_arr['jisi']?></p>
-						<p>职业：<?php echo $sql_arr['zhiye']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;学历：<?php echo $sql_arr['xueli']?></p>
-						<p>祭日：<?php echo $sql_arr['dieday']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;墓地：<?php echo $sql_arr['mudi']?></p>
-			墓地：<?php echo $sql_arr['mudi']?>
-		</p>
-		<p>
-			统计：<?php echo $sql_arr['tongji']?>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-			其它：<?php echo $sql_arr['other']?>
-		</p>
-          </td>
-        </tr>
-      </table>
-    </div>
-    <div class="text-center mt-3">
-      <a href="infoedit2.php?id=<?php echo $sql_arr['id'];?>" class="btn btn-primary mx-2">编辑</a>
-      <a href="index.php" class="btn btn-primary mx-2">返回主页</a>
-    </div>
-  </div>
+<?php
+session_start();
+require_once 'functions.php';
+
+// 验证用户是否已登录，未登录则跳转到登录页面
+if (!is_logged_in()) {
+    $url = 'login.php?url=' . urlencode($_SERVER['REQUEST_URI']);
+    redirect($url);
+}
+    require "../conn.php";
+    $id = $_GET['id'];
+    $sql = mysql_query("SELECT * FROM tree_lr WHERE id=$id",$link);
+    $sql_arr = mysql_fetch_assoc($sql);
+
+?>
+	<div class="tree well">
+	<div align=center>
+<form action="action-editinfo.php" method="post">
+    <label>刘<?php echo $sql_arr['name']?>编号:</label><input type="text" name="id" value="<?php echo $sql_arr['id']?>" readonly="readonly">
+    <label>刘<?php echo $sql_arr['name']?>父亲编号：</label><input type="text" name="pid" value="<?php echo $sql_arr['pid']?>" readonly="readonly">
+   <table border="0">
+    <tr>
+    <td> <label>姓名：</label></td><td><input type="text" name="name" value="<?php echo $sql_arr['name']?>"></td><td>因为都是刘氏子裔，仅需填名字，不代姓，两个字的名字，可将辈分带上，如刘林，长字辈，则可录入“林”或者“长林”</td></tr>
+     <tr>
+     <td><label>妻子：</label></td><td><input type="text" name="wname" value="<?php echo $sql_arr['wname']?>">
+     </td><td>不知道可填“无记载”或留空</td></tr>
+     <tr>
+     <td><label>出生时间：</label></td><td><input type="text" name="year" value="<?php echo $sql_arr['year']?>"></td><td>可填任意数字如“1987”“1987年”“一九八七”“戊戌年”</td></tr>
+     <tr>
+     <td><label>死亡时间：</label></td><td><input type="text" name="dieday" value="<?php echo $sql_arr['dieday']?>">  </td><td>在世不填，去世如不知道时间直接添“已去世”</td></tr>
+     <tr>
+     <td><label>墓地：</label></td><td><input type="text" name="mudi" value="<?php echo $sql_arr['mudi']?>">
+      </td><td>在世不填，去世墓地可填大概位置如葬于邻水县</td></tr>
+     <tr>
+     <td><label>出生地：</label></td><td><input type="text" name="birthadresse" value="<?php echo $sql_arr['birthadresse']?>"> </td><td>一般与父辈地址一致</td></tr>
+    <tr>
+    <td> <label>居住地：</label></td><td><input type="text" name="lifeadresse" value="<?php echo $sql_arr['lifeadresse']?>"> </td><td>定居地址</td></tr>
+     <tr>
+     <td><label>字辈：</label></td><td><input type="text" name="zibei" value="<?php echo $sql_arr['zibei']?>">    </td><td>价寿尚季圭、三仕应逢鼎、启志良心现、广发永长绵、宗强生贵子、圣王佑福卿、祖坐丰华地、万代庆兴隆</td></tr>
+     <tr>   <td><label>世代：</label></td><td><input type="text" name="dc" value="<?php echo $sql_arr['dc']?>">    </td><td>“绵”字辈20世代，以此类推，如“永”字辈18代，“宗字辈21世代，只填数字即可”</td></tr>
+    <tr>   <td> <label>生平简介：</label></td><td><input style="width:300%"type="text" name="info" value="<?php echo $sql_arr['info']?>"></td></tr><br>注意：以下所有数据修改如成功，点击“提交”按钮后不会有提示，指挥跳转到add.php页面，如不成功会提示“修改数据出错”
+
+</form>
+		</table>
+    <input type="submit" value="提交">
+   		</div>
+</div>
 </body>
 </html>
