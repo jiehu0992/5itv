397f1da4cb09f90fbe3d4da79cb3c3dc69215add
jiehu0992 <1517152664@qq.com>
2023-12-31 04:50:12 +0000

    样式文件
    Signed-off-by: jiehu0992 <1517152664@qq.com>

---
+++ css/header.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+	<div>
+		<ul class="nav navbar-nav">
+			<li><a href="/os.php">欧式族谱</a></li>		
+			<li><a href="/bt.php">塔式族谱</a></li>	
+			<li><a href="/search.php">搜索</a></li>
+		</ul>
+	</div>
+	</div>
+</nav>
